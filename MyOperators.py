f91d183a65f78343512f4652e675979d3fe97c43
fcadum <fadumg@gmail.com>
2024-07-12 11:49:19 -0300

    EXPRESSÕES COMPLEXAS

---
+++ MyOperators.py
@@ -0,0 +1,15 @@
+# Demonstração de expressões complexa
+
+# Demonstração de variáveis que iremos usar...
+x = 10; y = 20; z = 30
+
+valor = x * (y/10+z/10)-25
+print("O resultado da expressão #1 é: ", valor)
+
+valor = ((y-x)+(z-x))/3
+print("O resultado da expressão #2 é: ", valor)
+
+valor = (x + (y*2+(z/10)))-18
+print("O resultado da expressão #3 é: ", valor)
+
+#ANTES DE EXECUTAR ESTES CÓDIGOS, SERIA POSSÍVEL CALCULAR ESSE VALORES?
