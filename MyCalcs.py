c8c104405780b13bd21282743841d36d941f51a0
fcadum <fadumg@gmail.com>
2024-07-12 11:46:19 -0300

    APRENDENDO OPERAÇÕES NUMERICAS

---
+++ MyCalcs.py
@@ -0,0 +1,16 @@
+#Demonstração do uso de operadores...
+
+X = int(input("Digite um numero: "))
+Y = int(input("Digite outro número: "))
+
+soma = X + Y
+print("O resultado da operação é: ", soma)
+
+subtração = X - Y
+print("O resultado da subtração é:", subtração)
+
+multiplicação = X * Y
+print("O resultado da multiplicação é:", multiplicação)
+
+divisão = X / Y
+print("O resultado da divisão é: ", divisão)
