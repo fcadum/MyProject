028979f1f6b848da4740db4915da958ce512b866
fcadum <fadumg@gmail.com>
2024-07-17 09:52:22 -0300

    conteudo novo 17/07

--- conversortemperatura.py
+++ conversortemperatura.py
@@ -1,8 +1,27 @@
-c = int(input("Digite a temperatura em Celsius: "))
-k = int(input("Digite a temperatura em Kelvin: "))
-f = int(input("digite a temperatura em Fahrenheit: "))
+# Seria possível utilizar as estruturas condicionais if/elif/else ou match/case,
+# Para pernonalizar este programa, de forma que ele ofereç as três conversões?
+# Por exemplo, ele poderia perguntar ao usuário qual a unidae ed medida e qual valor de temperatura ele quer converter e a partir daí, realizar os cálculos necessários...
 
-celsiusparakelvin = c + 273
-kelvinparacelsius = k - 273
-celsiusparafahrenheit = c = (f − 32)/18
-fahrenheitparacelsius = c = (f − 32)/18
+print("Digite a temperatura que gostaria de converter... ")
+escolha = int(input("1. Celsius / 2. Kelvin / 3. Fahernheint"))
+temperatura = int(input("Digite o valor da temperatura: "))
+
+
+match escolha:
+    case 1:
+        kelvin = temperatura + 273
+        fahrenheit = (9 / 5 * temperatura) - 32
+        print(f"Aconversão de Celsius para Kelvin será {kelvin}.")
+        print(f"A conversão de Celsius para fahrenheit será {fahrenheit}")
+    case 2:
+        celsius = temperatura - 273
+        fahrenheit = * (temperatura - 273) + 32
+        print(f"A conversão de Kelvin para Celsius será {celsius}.")
+        print(f"A conversão de Kelvin para Fahrenheit será {fahrenheit}.")
+    case 3:
+        celsius =  5/9 * (temperatura - 32)
+        kelvin = (temperatura - 32) / 1.8 + 273
+        print(f"A temperatura de Fahrenheit para Celsius será {celsius}.")
+        print(f"A temperatura de Fahrenheit para Kelvin será {kelvin}")
+
+
---
+++ tarefassemanais.py
@@ -0,0 +1,31 @@
+      #Demonstração das tarefas dos dias da semana...
+print("Digite o dia da semama: ")
+print("2. Segunda-feira")
+print("3. Terça-feira")
+print("4. Quarta-feira")
+print("5. Quinta-feira")
+print("6. Sexta-feira")
+print("7. Sábado")
+print("8. Domingo")
+dia = int(input())
+
+
+match dia:
+    case 2:
+        print("Arrumar a sala.")
+    case 3:
+        print("Limpar a cozinha.")
+    case 4:
+        print("Organizar os armários.")
+    case 5:
+        print("Lavar o banheiro.")
+    case 6:
+        print("Arrumar os quartos.")
+    case 7:
+        print("Cortar a grama.")
+    case 8:
+        print("Limpar a piscina.")
+    case _:
+        print("Você precisa inserir um dia da semana válido!!")
+        
+print("Bom serviço!!!")
